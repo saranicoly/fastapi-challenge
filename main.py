4611aeab87af8c95efcfa9f8ec2238e1e5d6940f
Sara Lins <saranicoly0@gmail.com>
2022-06-19 18:39:34 -0300

    get possible friends

--- main.py
+++ main.py
@@ -12,20 +12,22 @@ def read_root():
   return show_dict(grafo)
 
 @app.get("/friends/{name}")
-def read_pessoa(name: str):
+def get_friends(name: str):
   all_friends = []
   for person, is_friend in grafo.matriz_adj[name].items():
     if is_friend == 1 and person != name:
       all_friends.append(person)
   return all_friends
 
-  # @app.get("/unknown_friends/{name}")
-  # def read_pessoa(name: str):
-  #   known_people = []
-  #   for known_person, is_knwon in grafo.matriz_adj[name].items():
-  #     if is_knwon == 1 and known_person != name:
-  #       known_people.append(known_person)
-  #   return known_people
+@app.get("/possible_friends/{name}")
+def get_possible_friends(name: str):
+  all_friends = set(get_friends(name))
+  all_friends.add(name)
+  possible_friends = set()
+  for person in all_friends:
+    temp_friends = set(get_friends(person))
+    possible_friends = possible_friends.union(temp_friends.difference(all_friends))
+  return possible_friends
 
 # @app.put("/items/{item_id}")
 # def update_item(item_id: int, item: Item):

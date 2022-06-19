f9d146aac06338749115afb267a4eae66d3506e0
Sara Lins <saranicoly0@gmail.com>
2022-06-19 17:47:02 -0300

    format graph

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import json
 from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from grafo import Grafo
 
 app = FastAPI()
+
+grafo = Grafo()
 
 class Item(BaseModel):
   name: str
@@ -12,7 +16,7 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-  return {"Hello": "World"}
+  return {show_dict(grafo)}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
@@ -21,3 +25,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
   return {"item_name": item.name, "item_id": item_id}
+
+def show_dict(grafo):
+  adj_list = list(grafo.matriz_adj.keys())
+  return json.dumps(adj_list, ensure_ascii=False).encode('utf8')

5d4596f5ceb5f05f817a6e41d4ba0cf87b866f0b
Sara Lins <saranicoly0@gmail.com>
2022-06-19 19:02:02 -0300

    create new person

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import json
 
+from pydantic import BaseModel
 from fastapi import FastAPI
 from grafo import Grafo
 
 app = FastAPI()
 
 grafo = Grafo()
+
+class Person(BaseModel):
+  name: str
+  friends: list = []
 
 @app.get("/")
 def read_root():
@@ -29,9 +34,13 @@ def get_possible_friends(name: str):
     possible_friends = possible_friends.union(temp_friends.difference(all_friends))
   return possible_friends
 
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#   return {"item_name": item.name, "item_id": item_id}
+@app.post("/add_person/")
+def create_person(person: Person):
+  grafo.matriz_adj[person.name] = {key: 0 for key in grafo.matriz_adj.keys()}
+  for friend in person.friends:
+    grafo.matriz_adj[person.name][friend] = 1
+    grafo.matriz_adj[friend][person.name] = 1
+  return {"Nova pessoa adicionada com sucesso!"}
 
 def show_dict(grafo):
   adj_list = list(grafo.matriz_adj.keys())

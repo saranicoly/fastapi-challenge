a4c7995496bd7d3f39d95c01929cf18dc756aef1
Sara Lins <saranicoly0@gmail.com>
2022-06-20 13:22:18 -0300

    add check_person method

--- app/grafo.py
+++ app/grafo.py
@@ -66,15 +66,15 @@ class Graph():
     self.matriz_adj[person.name] = {}
     for friend in person.friends:
       if friend not in self.matriz_adj:
-        raise HTTPException(status_code=404, detail=f"Friend {friend} not found")
+        raise HTTPException(status_code=404, detail=f"Friend {friend} not found. Try adding it first!")
       self.matriz_adj[person.name][friend] = 1
       self.matriz_adj[friend][person.name] = 1
     return "Pessoa adicionada com sucesso"
   
   def get_edges(self, name):
     # Shows the edges(friends) of a person
-    if name not in self.matriz_adj:
-      raise HTTPException(status_code=404, detail=f"Person {name} not found")
+    self.check_person(name)
+
     edges = []
     for friend in self.matriz_adj[name]:
       if self.matriz_adj[name][friend] == 1:
@@ -83,8 +83,8 @@ class Graph():
   
   def get_second_degree_edges(self, name):
     # Shows the second degree edges(friends of friends) of a person
-    if name not in self.matriz_adj:
-      raise HTTPException(status_code=404, detail=f"Person {name} not found")
+    self.check_person(name)
+
     edges = set(self.get_edges(name))
     edges.add(name) # Adds the person to the list of edges, so it won't be added in the second degree friends
     second_degree_edges = set()
@@ -92,3 +92,8 @@ class Graph():
       temp = set(self.get_edges(friend))
       second_degree_edges = second_degree_edges.union(temp.difference(edges))
     return second_degree_edges
+
+  def check_person(self, name):
+    # Checks if a person exists in the graph
+    if name not in self.matriz_adj:
+      raise HTTPException(status_code=404, detail=f"Person {name} not found. Try again!")

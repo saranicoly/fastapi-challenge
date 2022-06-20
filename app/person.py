55ca4a37d4f8f7c92ce5b97f9655b849eb027f2c
Sara Lins <saranicoly0@gmail.com>
2022-06-20 12:28:39 -0300

    remove initializer from Person class

--- app/person.py
+++ app/person.py
@@ -2,8 +2,4 @@ from pydantic import BaseModel
 
 class Person(BaseModel):
   name: str
-  friends: list = []
-
-  def __init__(self, name, friends=[]):
-    self.name = name
-    self.friends = friends
+  friends: list

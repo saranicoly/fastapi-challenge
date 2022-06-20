f5c787edadd8327ee21f1367866e9f25943dc955
Sara Lins <saranicoly0@gmail.com>
2022-06-20 12:11:42 -0300

    add show_graph and add_node to the Graph class

--- app/grafo.py
+++ app/grafo.py
@@ -1,3 +1,5 @@
+import json
+
 class Grafo():
   matriz_adj = dict()
 
@@ -11,11 +13,11 @@ class Grafo():
       "Vinicius": 1,
       "Carlos": 1,
       "João": 1,
-      "Ana": 1,
+      "Ana": 0,
       "Luiza": 0,
     }
     self.matriz_adj["Maria"] = {
-      "Maria": 1,
+      "Maria": 0,
       "Vinicius": 1,
       "Carlos": 0,
       "João": 0,
@@ -24,7 +26,7 @@ class Grafo():
     }
     self.matriz_adj["Vinicius"] = {
       "Maria": 1,
-      "Vinicius": 1,
+      "Vinicius": 0,
       "Carlos": 0,
       "João": 0,
       "Ana": 1,
@@ -36,21 +38,34 @@ class Grafo():
       "Carlos": 0,
       "João": 1,
       "Ana": 0,
-      "Luiza": 1,
+      "Luiza": 0,
     }
     self.matriz_adj["João"] = {
       "Maria": 0,
       "Vinicius": 0,
       "Carlos": 0,
-      "João": 1,
+      "João": 0,
       "Ana": 1,
       "Luiza": 1,
     }
     self.matriz_adj["Carlos"] = {
       "Maria": 0,
       "Vinicius": 0,
-      "Carlos": 1,
+      "Carlos": 0,
       "João": 0,
       "Ana": 1,
       "Luiza": 0,
     }
+
+  def show_graph(self):
+    # Shows the current state of the graph (all the people)
+    adj_list = list(self.matriz_adj.keys())
+    return json.dumps(adj_list, ensure_ascii=False).encode('utf8')
+  
+  def add_node(self, person):
+    # Adds a new person(node) to the graph
+    self.matriz_adj[person.name] = {key: 0 for key in self.matriz_adj.keys()}
+    for friend in person.friends:
+      self.matriz_adj[person.name][friend] = 1
+      self.matriz_adj[friend][person.name] = 1
+    return {"Nova pessoa adicionada com sucesso!"}
